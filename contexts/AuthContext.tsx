bea30f8031b9b1bcc50f16535674c935a298028e
Marcos Vinicius <marcao.adsistema@gmail.com>
2022-10-02 17:39:49 -0400

    feat: recuperando estado da autenticacao

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
-import { setCookie } from 'nookies'
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies } from 'nookies'
 import Router from "next/router";
 
 import { api } from "../services/api";
@@ -31,6 +31,18 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { 'appbasic.token': token } = parseCookies()
+
+    if (token) {
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data
+
+        setUser({ email, permissions, roles })
+      })
+    }
+  }, [])
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post("sessions", {
@@ -45,7 +57,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         path: '/',
       })
       
-      setCookie(undefined, 'appbasic.refreshToken', token, {
+      setCookie(undefined, 'appbasic.refreshToken', refreshToken, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: '/',
       })
@@ -56,6 +68,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         roles,
       });
 
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+ 
       Router.push("/dashboard");
     } catch (err) {
       console.log(err);
