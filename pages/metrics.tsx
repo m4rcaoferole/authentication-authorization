7f1ff6aaf015883aa987ffb229d301a4373343c9
Marcos Vinicius <marcao.adsistema@gmail.com>
2022-10-09 15:42:41 -0400

    feat: page metrics

---
+++ pages/metrics.tsx
@@ -0,0 +1,23 @@
+
+import { setupAPIClient } from "../services/api";
+import { withSSRAuth } from "../utils/withSSRAuth";
+
+export default function Metrics() {
+  return (
+    <>
+      <div>Métricas</div>
+    </>
+  );
+}
+
+// eslint-disable-next-line @next/next/no-typos
+export const getServerSidePros = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get("/me");
+
+  console.log(response.data);
+
+  return {
+    props: {},
+  };
+});
